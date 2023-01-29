796e6724c497b8c5491e57c61efd060271f4f47d
Carlos Manuel <fis.carlosmanuel@gmail.com>
2023-01-29 13:10:43 -0600

    Add python build type

---
+++ python/bindings.cpp
@@ -0,0 +1,17 @@
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+using namespace std;
+
+//*****************************
+//*         Bindings          *
+//****************************/
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(example, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("add", &add, "A function that adds two numbers");
+}
