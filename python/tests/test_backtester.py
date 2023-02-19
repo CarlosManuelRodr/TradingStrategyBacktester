f7cc750992377f22ba0fb1686f328f212fbd1d61
Carlos Manuel <fis.carlosmanuel@gmail.com>
2023-02-18 21:16:14 -0600

    Add python parallel test

--- python/tests/test_backtester.py
+++ python/tests/test_backtester.py
@@ -23,11 +23,30 @@ class TestBacktester(unittest.TestCase):
         strategy_program: str = 'Indicator("EMA", stock, time) < Indicator("ClosePrice", stock, time)'
         strategy_results: list[bool] = Evaluator.RunStrategy(strategy_program, "AAPL")
 
-        # noinspection PyTypeChecker
-        execution_data = Backtester.BacktestStoplossProfittake(strategy_results, "AAPL", 0.05, 0.05, 0.05, -1)
+        execution_data: list[ExecutionData] = Backtester.BacktestStoplossProfittake(strategy_results, "AAPL",
+                                                                                    0.05, 0.05, 0.05, -1)
         returns: list[float] = Returns.SimpleReturns(execution_data, 0.05)
 
         self.assertGreater(abs(sum(returns)), 0)
+
+    def test_parallel_backtester(self):
+        path = os.path.join(os.getcwd(), "..", "..", "dataset")
+        dataset: dict[str, StockData] = Loader.LoadDataset(path)
+        Evaluator.SetStrategyEvaluatorDataset(dataset)
+
+        strategy_program: str = 'Indicator("EMA", stock, time) < Indicator("ClosePrice", stock, time)'
+        strategy_results: dict[str, list[bool]] = Evaluator.RunStrategyAllStocks(strategy_program)
+
+        returns = []
+        for stock in strategy_results:
+            execution_data: list[ExecutionData] = Backtester.BacktestStoplossProfittake(strategy_results.get(stock),
+                                                                                        stock, 0.05, 0.05, 0.05, -1)
+            ret = sum(Returns.SimpleReturns(execution_data, 0.05))
+            print("Stock: " + stock + ", Return: " + str(ret))
+            returns.append(ret)
+
+        self.assertGreater(abs(sum(returns)), 0)
+
 
 
 if __name__ == '__main__':
